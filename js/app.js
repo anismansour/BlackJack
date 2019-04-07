9bce2d4cdf6420567fb43e0408b2994a6e290cfe
Anis Mansour <anismansour@Aniss-MacBook-Pro.local>
2019-04-06 17:26:20 -0700

    startingCss

--- js/app.js
+++ js/app.js
@@ -1,63 +1,63 @@
 const cards = [
   //spades
-  { name: "spadeOne", value1: 1, value: 11, picture: "" },
-  { name: "spadeTwo", value: 2, picture: "" },
-  { name: "spadeThree", value: 3, picture: "" },
-  { name: "spadeFour", value: 4, picture: "" },
-  { name: "spadeFive", value: 5, picture: "" },
-  { name: "spadeSix", value: 6, picture: "" },
-  { name: "spadeSeven", value: 7, picture: "" },
-  { name: "spadeEight", value: 8, picture: "" },
-  { name: "spadeNine", value: 9, picture: "" },
-  { name: "spadeTen", value: 10, picture: "" },
-  { name: "spadeJack", value: 10, picture: "" },
-  { name: "spadeQueen", value: 10, picture: "" },
-  { name: "spadeQueen", value: 10, picture: "" },
+  { name: "spadeOne", value1: 1, value: 11, picture: "card spades A" },
+  { name: "spadeTwo", value: 2, picture: "card spades r02" },
+  { name: "spadeThree", value: 3, picture: "card spades r03" },
+  { name: "spadeFour", value: 4, picture: "card spades r04" },
+  { name: "spadeFive", value: 5, picture: "card spades r05" },
+  { name: "spadeSix", value: 6, picture: "card spades r06" },
+  { name: "spadeSeven", value: 7, picture: "card spades r07" },
+  { name: "spadeEight", value: 8, picture: "card spades r08" },
+  { name: "spadeNine", value: 9, picture: "card spades r09" },
+  { name: "spadeTen", value: 10, picture: "card spades r10" },
+  { name: "spadeJack", value: 10, picture: "card spades J" },
+  { name: "spadeQueen", value: 10, picture: "card spades Q" },
+  { name: "spadeKing", value: 10, picture: "card spades K" },
 
   //hearts
-  { name: "heartOne", value1: 1, value: 11, picture: "" },
-  { name: "heartTwo", value: 2, picture: "" },
-  { name: "heartThree", value: 3, picture: "" },
-  { name: "heartFour", value: 4, picture: "" },
-  { name: "heartFive", value: 5, picture: "" },
-  { name: "heartSix", value: 6, picture: "" },
-  { name: "heartSeven", value: 7, picture: "" },
-  { name: "heartEight", value: 8, picture: "" },
-  { name: "heartNine", value: 9, picture: "" },
-  { name: "heartTen", value: 10, picture: "" },
-  { name: "heartJack", value: 10, picture: "" },
-  { name: "heartQueen", value: 10, picture: "" },
-  { name: "heartQueen", value: 10, picture: "" },
+  { name: "heartOne", value1: 1, value: 11, picture: "card hearts A" },
+  { name: "heartTwo", value: 2, picture: "card hearts r02" },
+  { name: "heartThree", value: 3, picture: "card hearts r03" },
+  { name: "heartFour", value: 4, picture: "card hearts r04" },
+  { name: "heartFive", value: 5, picture: "card hearts r05" },
+  { name: "heartSix", value: 6, picture: "card hearts r06" },
+  { name: "heartSeven", value: 7, picture: "card hearts r07" },
+  { name: "heartEight", value: 8, picture: "card hearts r08" },
+  { name: "heartNine", value: 9, picture: "card hearts r09" },
+  { name: "heartTen", value: 10, picture: "card hearts r10" },
+  { name: "heartJack", value: 10, picture: "card hearts J" },
+  { name: "heartQueen", value: 10, picture: "card hearts Q" },
+  { name: "heartKing", value: 10, picture: "card hearts K" },
   //Clubs
-  { name: "clubOne", value1: 1, value: 11, picture: "" },
-  { name: "clubTwo", value: 2, picture: "" },
-  { name: "clubThree", value: 3, picture: "" },
-  { name: "clubFour", value: 4, picture: "" },
-  { name: "clubFive", value: 5, picture: "" },
-  { name: "clubSix", value: 6, picture: "" },
-  { name: "clubSeven", value: 7, picture: "" },
-  { name: "clubEight", value: 8, picture: "" },
-  { name: "clubNine", value: 9, picture: "" },
-  { name: "clubTen", value: 10, picture: "" },
-  { name: "clubJack", value: 10, picture: "" },
-  { name: "clubQueen", value: 10, picture: "" },
-  { name: "clubQueen", value: 10, picture: "" },
+  { name: "clubOne", value1: 1, value: 11, picture: "card clubs A" },
+  { name: "clubTwo", value: 2, picture: "card clubs r02" },
+  { name: "clubThree", value: 3, picture: "card clubs r03" },
+  { name: "clubFour", value: 4, picture: "card clubs r04" },
+  { name: "clubFive", value: 5, picture: "card clubs r05" },
+  { name: "clubSix", value: 6, picture: "card clubs r06" },
+  { name: "clubSeven", value: 7, picture: "card clubs r07" },
+  { name: "clubEight", value: 8, picture: "card clubs r08" },
+  { name: "clubNine", value: 9, picture: "card clubs r09" },
+  { name: "clubTen", value: 10, picture: "card clubs r10" },
+  { name: "clubJack", value: 10, picture: "card clubs J" },
+  { name: "clubQueen", value: 10, picture: "card clubs Q" },
+  { name: "clubKing", value: 10, picture: "card clubs K" },
 
   //Diamonds
 
-  { name: "diamondOne", value1: 1, value: 11, picture: "" },
-  { name: "diamondTwo", value: 2, picture: "" },
-  { name: "diamondThree", value: 3, picture: "" },
-  { name: "diamondFour", value: 4, picture: "" },
-  { name: "diamondFive", value: 5, picture: "" },
-  { name: "diamondSix", value: 6, picture: "" },
-  { name: "diamondSeven", value: 7, picture: "" },
-  { name: "diamondEight", value: 8, picture: "" },
-  { name: "diamondNine", value: 9, picture: "" },
-  { name: "diamondTen", value: 10, picture: "" },
-  { name: "diamondJack", value: 10, picture: "" },
-  { name: "diamondQueen", value: 10, picture: "" },
-  { name: "diamondQueen", value: 10, picture: "" }
+  { name: "diamondOne", value1: 1, value: 11, picture: "card diamonds A" },
+  { name: "diamondTwo", value: 2, picture: "card diamonds r02" },
+  { name: "diamondThree", value: 3, picture: "card diamonds r03" },
+  { name: "diamondFour", value: 4, picture: "card diamonds r04" },
+  { name: "diamondFive", value: 5, picture: "card diamonds r05" },
+  { name: "diamondSix", value: 6, picture: "card diamonds r06" },
+  { name: "diamondSeven", value: 7, picture: "card diamonds r07" },
+  { name: "diamondEight", value: 8, picture: "card diamonds r08" },
+  { name: "diamondNine", value: 9, picture: "card diamonds r09" },
+  { name: "diamondTen", value: 10, picture: "card diamonds r10" },
+  { name: "diamondJack", value: 10, picture: "card diamonds J" },
+  { name: "diamondQueen", value: 10, picture: "card diamonds Q" },
+  { name: "diamondKing", value: 10, picture: "card diamonds K" }
 ];
 
 class Player {
@@ -96,6 +96,58 @@ const TheGame = () => {
   };
   initPlayer();
 
+  const dealerHit = () => {
+    document.getElementById("dealerCard2").className =
+      dealer.cardsInHand[1].picture;
+    while (dealer.valueOfcards < 17) {
+      var div = document.body.appendChild(document.createElement("div"));
+      div.className = shuffle[0].picture;
+      dealer.cardsInHand.push(shuffle[0]);
+      shuffle.splice(0, 1);
+      dealer.valueOfcards = 0;
+      checkDealerValue();
+    }
+  };
+
+  //button hit function
+  function playerHit() {
+    //
+    var div = document.body.appendChild(document.createElement("div"));
+    //
+    player1.cardsInHand.push(shuffle[0]);
+    div.className = shuffle[0].picture;
+    shuffle.splice(0, 1);
+    player1.valueOfcards = 0;
+    checkPlayerValue();
+  }
+
+  //button stand function
+  function standCard() {
+    document.getElementById("dealerCard2").className =
+      dealer.cardsInHand[1].picture;
+    if (
+      dealer.valueOfcards < 17 &&
+      dealer.valueOfcards > player1.valueOfcards
+    ) {
+      return alert("dealer won");
+    }
+    if (dealer.valueOfcards >= 17) {
+      if (dealer.valueOfcards > player1.valueOfcards) {
+        return alert("the dealer Won inside the stand button " + bank);
+      }
+      if (player1.valueOfcards > dealer.valueOfcards) {
+        bank = bank + bet * 1.5;
+        alert("the Player won inside the stand button  " + bank);
+      }
+      if (player1.valueOfcards === dealer.valueOfcards) {
+        bank = bank + bet;
+        alert(" no winner  " + bank);
+      }
+    } else {
+      dealerHit();
+    }
+  }
+
   //console.log("the palyer1 is  ");
   //console.log(player1);
 
@@ -126,36 +178,53 @@ const TheGame = () => {
   };
 
   bets();
-  console.log("the bet amount before game " + bet);
-  console.log("the bank amount before game " + bank);
+  //console.log("the bet amount before game " + bet);
+  //console.log("the bank amount before game " + bank);
   //function to deal the cards from the shuffled cards
   const firstDeal = () => {
     //while cards are not 2 on each hand
     while (dealer.cardsInHand.length < 2 || player1.cardsInHand.length < 2) {
-      //   player1.cardsInHand.push(shuffle[0]);
-      //   shuffle.splice(0, 1);
-      //   dealer.cardsInHand.push(shuffle[0]);
-      //   shuffle.splice(0, 1);
+      player1.cardsInHand.push(shuffle[0]);
+      shuffle.splice(0, 1);
+      dealer.cardsInHand.push(shuffle[0]);
+      shuffle.splice(0, 1);
 
-      dealer.cardsInHand[0] = {
-        name: "diamondOne",
-        value1: 1,
-        value: 11,
-        picture: ""
-      };
-      dealer.cardsInHand[1] = { name: "diamondJack", value: 10, picture: "" };
+      // !!!!!!!!!!to test game with preset cards !!!!!!!!!
+      // dealer.cardsInHand[1] = { name: "diamondNine", value: 9, picture: "" };
 
-      player1.cardsInHand[0] = {
-        name: "spadeOne",
-        value1: 1,
-        value: 11,
-        picture: ""
-      };
-      player1.cardsInHand[1] = { name: "spadeJack", value: 10, picture: "" };
+      //   player1.cardsInHand[0] = {
+      //     name: "spadeOne",
+      //     value1: 1,
+      //     value: 11,
+      //     picture: ""
+      //   };
+      //   player1.cardsInHand[1] = { name: "spadeJack", value: 10, picture: "" };
     }
   };
   firstDeal();
 
+  document.getElementById("dealerCard1").className =
+    dealer.cardsInHand[0].picture;
+  document.getElementById("playerCard1").className =
+    player1.cardsInHand[0].picture;
+  document.getElementById("playerCard2").className =
+    player1.cardsInHand[1].picture;
+
+  const hideButtons = () => {
+    if (player1.valueOfcards < 21) {
+      document.getElementById("buttonHit").style.visibility = "visible";
+      document.getElementById("buttonStand").style.visibility = "visible";
+    } else {
+      document.getElementById("buttonHit").style.visibility = "hidden";
+      document.getElementById("buttonStand").style.visibility = "hidden";
+    }
+  };
+
+  const checkPlayerLoss = () => {
+    if (dealer.valueOfcards > 21) {
+      return alert("Player lost");
+    }
+  };
   const checkPlayerValue = () => {
     for (let i = 0; i < player1.cardsInHand.length; i++) {
       if (
@@ -177,16 +246,15 @@ const TheGame = () => {
           player1.valueOfcards + player1.cardsInHand[i].value;
       }
     }
+    hideButtons();
+    checkPlayerLoss();
   };
   checkPlayerValue();
-
-  // console.log("the value of the cards are   " + player1.valueOfcards);
 
   //If the dealer has an ace, and counting it as 11 would bring his total to 17 or more (but not over 21),
   //he must count the ace as 11 and stand. The dealer's decisions, then, are automatic on all plays,
   //whereas the player always has the option of taking one or more cards.
 
-  console.log("the value of the dealer card is " + dealer.cardsInHand[0].value);
   const checkAce = () => {
     // if the total is more than 21 we check if there is an ace. is yes we change it to 1.
     for (let i = 0; i < dealer.cardsInHand.length; i++) {
@@ -215,23 +283,27 @@ const TheGame = () => {
   };
   checkDealerValue();
 
-  const checkNaturalsWin = () => {
+  const checkFirstGame = () => {
     if (player1.valueOfcards === 21 && dealer.valueOfcards === 21) {
       console.log("both won, both have a black jack");
       bank = bank + bet;
+      return bank;
     }
     if (player1.valueOfcards === 21 && dealer.valueOfcards < 21) {
       console.log("player won with Blackjack");
       bank = bank + bet * 1.5;
+      return bank;
     }
-    // if (dealer.valueOfcards === 21 && player1.valueOfcards < 21)
-    //   console.log("dealer has blackJack");
-    // bank = bank + bet;
-  };
-  checkNaturalsWin();
-  console.log("the bank is   " + bank);
-  console.log("the bet is   " + bet);
-};
 
-const checkNextMove = () => {};
+    checkFirstGame();
+
+    //console.log("the bank is   " + bank);
+    //console.log("the bet is   " + bet);
+  };
+  document.getElementById("buttonHit").addEventListener("click", playerHit);
+  document.getElementById("buttonStand").addEventListener("click", standCard);
+
+  console.log(player1);
+  console.log(dealer);
+};
 TheGame();
